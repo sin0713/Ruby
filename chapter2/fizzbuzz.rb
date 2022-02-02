f0b85be05bd43588614e1f0b5854b8bac1e2db01
sin0713 <narisin1124@gmail.com>
2022-02-02 22:34:36 +0900

    [Add]

---
+++ chapter2/fizzbuzz.rb
@@ -0,0 +1,17 @@
+
+N = gets.to_i
+
+for i in 1..N
+  puts "i = #{i}"
+  if i % 15 == 0
+    puts 'FizzBuzz'
+  elsif i % 3 == 0
+    puts 'Fizz'
+  elsif i % 5 == 0
+    puts 'Buzz'
+  else
+    puts i
+  end
+end
+
+
